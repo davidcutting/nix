a89046733da72f37d011af7114e9d4d9a60f553c
dcutting <dcutting@umich.edu>
2024-06-29 17:43:19 -0400

    Add cpp nix dev shell

---
+++ develop/flake.nix.cpp
@@ -0,0 +1,45 @@
+{
+  description = "C++ Development with Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # This is the list of architectures that work with this project
+      systems = [
+        "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"
+      ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+
+        # devShells.default describes the default shell with C++, cmake, boost,
+        # and catch2
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              gtest
+              meson
+              ninja
+            ];
+          };
+
+          # macOS users use pkgs.gccStdenv instead of pkgs.clangStdenv
+
+          clang = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+            packages = with pkgs; [
+              clang-tools
+              llvm
+              lcov
+              gdb
+              gtest
+              gcovr
+              meson
+              ninja
+              wget
+            ];
+          };
+        };
+      };
+    };
+}
